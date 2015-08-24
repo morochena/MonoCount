56a715487b4e5c232ed47ea2f5fe042a31bef9ee
Marcus Orochena <marcus@orochena.net>
2015-08-24 11:51:24 -0400

    GameMenu.js converted to ES6 using AirBNB style guide

--- App/Components/GameMenu.js
+++ App/Components/GameMenu.js
@@ -1,70 +1,84 @@
-var React = require('react-native');
+const React = require('react-native');
 
-var {
-    View,
-    Text,
-    StyleSheet,
-    AlertIOS,
-    TouchableHighlight
+const {
+  View,
+  Text,
+  StyleSheet,
+  AlertIOS,
+  TouchableHighlight,
 } = React;
 
-var Options = require('./Options');
+const Options = require('./Options');
 
-var GameMenu = React.createClass({
-
-    goBack: function() {
-        AlertIOS.alert(
-            'Back to Main Menu',
-            'Your current game will be lost.',
-            [
-                {text: 'Yes', onPress: () => this.props.navigator.pop()},
-                {text: 'Cancel', onPress: () => console.log('cancelled')}
-            ]
-        );
-    },
-
-    goOptions() {
-        this.props.navigator.push({component: Options});
-    },
-
-    render: function() {
-        return(
-                <View style={styles.container}>
-
-                    <TouchableHighlight style={styles.button}>
-                        <Text onPress={this.props.resetHealth} style={styles.gg}> Reset </Text>
-                    </TouchableHighlight>
-
-                    <TouchableHighlight style={styles.button} onPress={this.goBack}>
-                        <Text onPress={this.goBack} style={styles.gg}> Menu </Text>
-                    </TouchableHighlight>
-
-                    <TouchableHighlight style={styles.button} onPress={this.options}>
-                        <Text onPress={this.goOptions} style={styles.gg}> Options </Text>
-                    </TouchableHighlight>
-
-                </View>
-        );
-    }
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  smcont: {
+    flex: 1,
+  },
+  gg: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+  button: {
+    flex: 1,
+    padding: 5,
+  },
 });
 
-var styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    smcont: {
-        flex: 1
-    },
-    gg: {
-        textAlign: 'center',
-        color: '#fff'
-    },
-    button: {
-        flex: 1,
-        padding: 5
-    }
-});
+class GameMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.goBack = this.goBack.bind(this);
+    this.goOptions = this.goOptions.bind(this);
+    this.state = {};
+  }
+
+  goBack() {
+    AlertIOS.alert(
+      'Back to Main Menu',
+      'Your current game will be lost.',
+      [
+        {text: 'Yes', onPress: () => this.props.navigator.pop()},
+        {text: 'Cancel', onPress: () => ''},
+      ]
+    );
+  }
+
+  goOptions() {
+    this.props.navigator.push({component: Options});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.button}>
+          <Text onPress={this.props.resetHealth} style={styles.gg}> Reset </Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.button} onPress={this.goBack}>
+          <Text onPress={this.goBack} style={styles.gg}> Menu </Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.button} onPress={this.options}>
+          <Text onPress={this.goOptions} style={styles.gg}> Options </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+GameMenu.propTypes = {
+  navigator: React.PropTypes.number,
+  resetHealth: React.PropTypes.number,
+};
+
+GameMenu.defaultProps = {
+  navigator: 5,
+  resetHealth: 5,
+};
 
 module.exports = GameMenu;
